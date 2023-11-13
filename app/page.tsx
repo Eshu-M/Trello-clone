e203ab868c3b195c0256d2f0d446def6731b5bbd
Esham Mubarek <eshambehredin20@gmail.com>
2023-11-13 14:48:04 -0800

    completed the Ui of the header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="">
-        Hello World
-      </h1>
+    <main>
+      {/* Header */}
+      <Header/>
     </main>
   )
 }
